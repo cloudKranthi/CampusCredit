e53923a9f8dc4f412a3b641ec53e8ad8d9969b5b
cloudkranthi <shivakranthi0006@gmail.com>
2026-02-20 18:26:22 +0530

    added BaseEntity class

---
+++ src/main/java/com/college/wallet/model/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.college.wallet.model;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
+@MappedSuperclass
+@Getter
+@Setter
+public  abstract  class BaseEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
+    @Version
+    private Long version;
+    @CreationTimestamp
+    @Column(updatable=false,nullable=false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
